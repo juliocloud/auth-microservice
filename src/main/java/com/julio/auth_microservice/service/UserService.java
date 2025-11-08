0d6ab6cff303ec6d05303affe40fb87eadc20cce
juliocloud <juliocesarjuriolli@gmail.com>
2025-11-08 11:55:01 -0300

    feat: user service

--- src/main/java/com/julio/auth_microservice/service/UserService.java
+++ src/main/java/com/julio/auth_microservice/service/UserService.java
@@ -1,4 +1,19 @@
 package com.julio.auth_microservice.service;
 
+import com.julio.auth_microservice.model.User;
+import com.julio.auth_microservice.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private UserRepository userRepository;
+
+    public void create(User user){
+        this.userRepository.save(user);
+    }
 }
