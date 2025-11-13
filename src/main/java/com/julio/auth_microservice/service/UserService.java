be62e47aaf61a51a7768042abb0b69f0b60e11bb
juliocloud <juliocesarjuriolli@gmail.com>
2025-11-13 20:53:12 -0300

    fix: making userrepo final

--- src/main/java/com/julio/auth_microservice/service/UserService.java
+++ src/main/java/com/julio/auth_microservice/service/UserService.java
@@ -2,16 +2,17 @@ package com.julio.auth_microservice.service;
 
 import com.julio.auth_microservice.model.User;
 import com.julio.auth_microservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
-    private UserRepository userRepository;
 
     public void create(User user){
         this.userRepository.save(user);
