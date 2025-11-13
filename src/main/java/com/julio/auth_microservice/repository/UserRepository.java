4ab7851294f3ecc928e62eaf198a113a7050e5fa
juliocloud <juliocesarjuriolli@gmail.com>
2025-11-13 20:53:27 -0300

    fix: making userrepository an interface

--- src/main/java/com/julio/auth_microservice/repository/UserRepository.java
+++ src/main/java/com/julio/auth_microservice/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package com.julio.auth_microservice.repository;
 
 import com.julio.auth_microservice.model.User;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public abstract class UserRepository implements JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
