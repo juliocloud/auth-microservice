50c1e279225261cf7e507d64a005ee4a573c2240
juliocloud <juliocesarjuriolli@gmail.com>
2025-11-13 21:03:43 -0300

    feat: auth and user crud

--- src/main/java/com/julio/auth_microservice/controller/UserController.java
+++ src/main/java/com/julio/auth_microservice/controller/UserController.java
@@ -4,13 +4,14 @@ import com.julio.auth_microservice.model.User;
 import com.julio.auth_microservice.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("/auth")
 public class UserController {
 
     private UserService userService;
@@ -23,5 +24,16 @@ public class UserController {
     public ResponseEntity create(User user){
         this.userService.create(user);
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "This endpoint is not secure";
+    }
+
+    @GetMapping("/admin/adminProfile")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminProfile(){
+        return "This is an admin route";
     }
 }
