5fc40815f8f959381967c2f6c2277ae3cd27f1f8
juliocloud <juliocesarjuriolli@gmail.com>
2025-11-20 12:52:14 -0300

    feat: implementing duplicate users checkup

--- src/main/java/com/julio/auth_microservice/service/UserService.java
+++ src/main/java/com/julio/auth_microservice/service/UserService.java
@@ -1,8 +1,13 @@
 package com.julio.auth_microservice.service;
 
+import com.julio.auth_microservice.exception.UserAlreadyExistException;
 import com.julio.auth_microservice.model.User;
 import com.julio.auth_microservice.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -13,7 +18,13 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void create(User user){
+    public Optional<User> getByUserName(String email){
+        return this.userRepository.findUserByEmail(email);
+    }
+
+    public void create(User user) {
+        if(this.getByUserName(user.getEmail()).isPresent())
+            throw new UserAlreadyExistException();
         this.userRepository.save(user);
     }
 }
